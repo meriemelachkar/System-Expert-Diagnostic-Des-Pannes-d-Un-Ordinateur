5ad055f95cddf4a05b7411c470a0f1a742468f8f
Merieem22 <143353403+meriemelachkar@users.noreply.github.com>
2024-03-31 01:34:35 +0000

    Add files via upload

---
+++ listQuestions.js
@@ -0,0 +1,11 @@
+var questionPanneOrdinateur = [
+    "L'ordinateur ne s'allume pas ?",
+    "Un bruit est émis par le ventilateur ?",
+    "Un écran noir apparaît au démarrage ?",
+    "L'ordinateur est lent ?",
+    "Vous rencontrez des problèmes de connexion Internet ?",
+    "Vous rencontrez des problèmes audio ?",
+    "Le système est gelé ?",
+    "Vous rencontrez un écran bleu de la mort (BSOD) ?",
+    "Vous soupçonnez une défaillance matérielle ?"
+  ];
